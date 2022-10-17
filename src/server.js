f883f295f61bcbd86e51fb58499a0186f0857b0b
bharathaladandi <bharathaladandi@gmail.com>
2022-10-17 19:45:36 +0530

    Basic Structure Added

--- src/server.js
+++ src/server.js
@@ -1,11 +1,17 @@
 const express = require('express');
-
+const morgan = require('morgan');
 
 
 const port = 5000;
 
+app.get("/", (req, res) => {
+    res.send("Home Page");
+})
 
 
+app.get("/about", (req, res) => {
+    res.send("About Page");
+})
 
 const app = express();
 
